cf3bfff48b515a4c49ce2d8004b4ee1443d2ea21
ManfredHu <manfredHuFE@gmail.com>
2022-04-25 15:05:57 +0800

    Revert "Update index.tsx"
    This reverts commit 4e31964925b58b8b444595ecee7634193e3b4729.

--- src/cart/index.tsx
+++ src/cart/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import { List, Typography } from "antd"
 import ItemCard from "./ItemCard"
+import { List, Typography } from "antd"
 import { useChecked } from "./use-checked"
 import "./index.css"
 
